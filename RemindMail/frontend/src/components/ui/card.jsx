2261c6f9c0bd62dbb288da0466ac09218c685b20
Luciano Pereira <lucianop.jogador@gmail.com>
2026-03-17 02:40:06 -0300

    Delete RemindMail directory

--- RemindMail/frontend/src/components/ui/card.jsx
+++
@@ -1,7 +0,0 @@
-export function Card({ children, className }) {
-  return <div className={`border rounded-xl bg-white shadow ${className}`}>{children}</div>;
-}
-
-export function CardContent({ children, className }) {
-  return <div className={`p-4 ${className}`}>{children}</div>;
-}
