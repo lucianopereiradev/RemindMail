013ea7aa2d55470aa6e28a915970144c326a6ef5
Luciano Pereira <lucianop.jogador@gmail.com>
2026-03-17 01:07:29 -0300

    Add files via upload

--- RemindMail/backend/server.js
+++ RemindMail/backend/server.js
@@ -12,13 +12,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+      }
+);
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
